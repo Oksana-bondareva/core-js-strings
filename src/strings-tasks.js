f7f74f176ae348e7eada5921e707de69f502b68e
Oksana-bondareva <oksanchik252@gmail.com>
2023-11-16 17:15:50 +0300

    fix: correct linter error

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -152,8 +152,9 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const stringIndex = str.indexOf(value);
+  return str.replace(str.slice(stringIndex, stringIndex + value.length), '');
 }
 
 /**
